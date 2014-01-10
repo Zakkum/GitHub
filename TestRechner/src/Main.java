4aaf7cfc828cde48f416099f713d95afd61c812b
Zakkum <anilguney@hotmail.de>
2014-01-10 17:47:09 +0100

    Hallo wie gehts

---
+++ TestRechner/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	
+
+	public static void main(String[] args) {
+		
+		int return10Ct;
+		int balance=35;
+		int fill10Ct=50;
+		
+		return10Ct = balance / 10;
+		System.out.println(return10Ct);
+		balance = balance - return10Ct*10;
+		System.out.println(balance);
+		
+
+	}
+
+}
